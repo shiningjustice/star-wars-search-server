ee1883f7c3ca0483376be0e6b46bdb74d874736b
Phoebe Law <phoebebasilio@gmail.com>
2020-04-04 12:15:15 -0700

    adds user table schema and seeds

---
+++ seeds/seeds.table.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+TRUNCATE
+  "user";
+
+INSERT INTO "user" (("id", "username", "password", "first_name", "side")
+VALUES
+  (
+    1,
+    'admin'
+    'Admin', 
+    -- password = "P@ssw0rd", salted by 12
+    '$2a$12$97iynXdhkTqyWYjlISIIbevz0gPEq0QX7RstxZji87kMN.bdn01l2',
+  );
+
+COMMIT;
