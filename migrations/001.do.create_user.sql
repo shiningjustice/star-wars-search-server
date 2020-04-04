54b0c600d5d55f730a3eb3972e151243be08ee01
Phoebe Law <phoebebasilio@gmail.com>
2020-04-04 12:30:05 -0700

    fixes typo

--- migrations/001.do.create_user.sql
+++ migrations/001.do.create_user.sql
@@ -1,7 +1,7 @@
-CREATE TABLE "user" {
+CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY, 
   "username" TEXT NOT NULL UNIQUE, 
   "password" TEXT NOT NULL,
   "first_name" TEXT NOT NULL,
   "side" TEXT DEFAULT 'light-side'
-}
+)
