aad26d80df03e3026fe848482e6e0e1414ac2a2b
Phoebe Law <phoebebasilio@gmail.com>
2020-04-04 12:14:49 -0700

    adds db setup (incl scripts) and other env secrets

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-//sets defaults for values in process.env, if unavailable
-
 module.exports = {
   PORT: process.env.PORT || 8000, 
   NODE_ENV: process.env.NODE_ENV || 'development', 
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://admin@localhost/starwars',
+  JWT_SECRET: process.env.JWT_SECRET || 'youshouldprobablychangeme',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
